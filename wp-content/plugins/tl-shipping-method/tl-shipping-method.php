a4ef71fa8215bcf439a0d7c4f6e1965769d8b464
Therese Linden <theresemlinden@gmail.com>
2023-04-24 13:00:10 +0200

    removed unused code

--- wp-content/plugins/tl-shipping-method/tl-shipping-method.php
+++ wp-content/plugins/tl-shipping-method/tl-shipping-method.php
@@ -107,56 +107,6 @@ if (in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_
               'desc_tip'    => true,
             ),
           );
-
-          // $shipping_classes = WC()->shipping()->get_shipping_classes();
-          // $cost_desc = 'Enter cost for this shipping class';
-
-
-          // if (!empty($shipping_classes)) {
-          //   $this->instance_form_fields['class_costs'] = array(
-          //     'title'       => __('Shipping class costs', 'woocommerce'),
-          //     'type'        => 'title',
-          //     'default'     => '',
-          //     /* translators: %s: URL for link. */
-          //     'description' => sprintf(__('These costs can optionally be added based on the <a href="%s">product shipping class</a>.', 'woocommerce'), admin_url('admin.php?page=wc-settings&tab=shipping&section=classes')),
-          //   );
-          //   foreach ($shipping_classes as $shipping_class) {
-          //     if (!isset($shipping_class->term_id)) {
-          //       continue;
-          //     }
-          //     $this->instance_form_fields['class_cost_' . $shipping_class->term_id] = array(
-          //       /* translators: %s: shipping class name */
-          //       'title'             => sprintf(__('"%s" shipping class cost', 'woocommerce'), esc_html($shipping_class->name)),
-          //       'type'              => 'text',
-          //       'placeholder'       => __('N/A', 'woocommerce'),
-          //       'description'       => $cost_desc,
-          //       'default'           => $this->get_option('class_cost_' . $shipping_class->slug), // Before 2.5.0, we used slug here which caused issues with long setting names.
-          //       'desc_tip'          => true,
-          //       //'sanitize_callback' => array($this, 'sanitize_cost'),
-          //     );
-          //   }
-
-          //   $this->instance_form_fields['no_class_cost'] = array(
-          //     'title'             => __('No shipping class cost', 'woocommerce'),
-          //     'type'              => 'text',
-          //     'placeholder'       => __('N/A', 'woocommerce'),
-          //     'description'       => $cost_desc,
-          //     'default'           => '',
-          //     'desc_tip'          => true,
-          //     // 'sanitize_callback' => array($this, 'sanitize_cost'),
-          //   );
-
-          //   $this->instance_form_fields['type'] = array(
-          //     'title'   => __('Calculation type', 'woocommerce'),
-          //     'type'    => 'select',
-          //     'class'   => 'wc-enhanced-select',
-          //     'default' => 'class',
-          //     'options' => array(
-          //       'class' => __('Per class: Charge shipping for each shipping class individually', 'woocommerce'),
-          //       'order' => __('Per order: Charge shipping for the most expensive shipping class', 'woocommerce'),
-          //     ),
-          //   );
-          // }
         } // End instance_init_form_fields()
 
         /**
@@ -173,68 +123,8 @@ if (in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_
             'label' => $this->title,
             'cost' =>  $this->cost,
             'package' => $package,
-            //'calc_tax' => 'per_item'
           );
-          print_r($package['contents']);
-
-          // // Register the rate
           $this->add_rate($rate);
-
-          // $rate = array(
-          //   'id'      => $this->get_rate_id(),
-          //   'label'   => $this->title,
-          //   'cost'    => $this->get_option('cost'),
-          //   'package' => $package,
-          // );
-
-          // // Calculate the costs.
-          // $has_costs = false; // True when a cost is set. False if all costs are blank strings.
-          // $cost      = $this->get_option('cost');
-
-          // if ('' !== $cost) {
-          //   $has_costs    = true;
-          // }
-
-          // // Add shipping class costs.
-          // $shipping_classes = WC()->shipping()->get_shipping_classes();
-
-          // if (!empty($shipping_classes)) {
-          //   $found_shipping_classes = $this->find_shipping_classes($package);
-          //   $highest_class_cost     = 0;
-
-          //   foreach ($found_shipping_classes as $shipping_class => $products) {
-          //     // Also handles BW compatibility when slugs were used instead of ids.
-          //     $shipping_class_term = get_term_by('slug', $shipping_class, 'product_shipping_class');
-          //     $class_cost_string   = $shipping_class_term && $shipping_class_term->term_id ? $this->get_option('class_cost_' . $shipping_class_term->term_id, $this->get_option('class_cost_' . $shipping_class, '')) : $this->get_option('no_class_cost', '');
-
-          //     if ('' === $class_cost_string) {
-          //       continue;
-          //     }
-
-          //     $has_costs  = true;
-          //     $class_cost = $this->evaluate_cost(
-          //       $class_cost_string,
-          //       array(
-          //         'qty'  => array_sum(wp_list_pluck($products, 'quantity')),
-          //         'cost' => array_sum(wp_list_pluck($products, 'line_total')),
-          //       )
-          //     );
-
-          //     if ('class' === $this->type) {
-          //       $rate['cost'] += $class_cost;
-          //     } else {
-          //       $highest_class_cost = $class_cost > $highest_class_cost ? $class_cost : $highest_class_cost;
-          //     }
-          //   }
-
-          //   if ('order' === $this->type && $highest_class_cost) {
-          //     $rate['cost'] += $highest_class_cost;
-          //   }
-          // }
-
-          // if ($has_costs) {
-          //   $this->add_rate($rate);
-          // }
         }
       }
     }
